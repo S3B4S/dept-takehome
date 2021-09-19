64d695fae49ddc23348e7d112bfa773d8c782f3d
Kevin van der Werff <kevinsebastianate@hotmail.com>
2021-09-19 17:02:49 +0200

    use Image from next/image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Hero.module.css"
+import Image from "next/image"
 
 const Hero = () => {
   return (
@@ -6,10 +7,10 @@ const Hero = () => {
       <div className={styles.container}>
         <div>
           <nav className={styles.nav}>
-            <img className={styles.logo} src="/icons/icon-dept.svg" alt="Dept logo" />
+            <Image src="/icons/icon-dept.svg" alt="Dept logo" height="20px" width="60px"/>
             <div className={styles.navItems}>
               <span className={styles.menuLabel}>Menu</span>
-              <img src="/icons/icon-menu.svg" alt="Menu" />
+              <Image src="/icons/icon-menu.svg" alt="Menu" height="10px" width="20px" />
             </div>
           </nav>
           <span className={styles.callout}>Work</span>
