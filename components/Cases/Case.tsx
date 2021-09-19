afab034f29891cc2be4c5e1ddf37b89d84d07e13
Kevin van der Werff <kevinsebastianate@hotmail.com>
2021-09-19 19:16:51 +0200

    linter fixes, empty alt tag for decorative image

--- components/Cases/Case.tsx
+++ components/Cases/Case.tsx
@@ -6,7 +6,7 @@ import { HTMLAttributes } from "react"
 const Case = ({ clientCase, showCover = true, className = "" }: { clientCase: ClientCase, showCover?: boolean } & HTMLAttributes<HTMLDivElement>) => {
   return (
     <section className={styles.case + " " + className}>
-      {showCover && <Image src={clientCase.cover_image} height="500px" width="500px" className={styles.caseCover}/>}
+      {showCover && <Image src={clientCase.cover_image} alt="" height="500px" width="500px" className={styles.caseCover}/>}
       <span className={styles.name}>{clientCase.client}</span>
       <span className={styles.title}>{clientCase.title}</span>
       <span className={styles.open}>View case</span>
