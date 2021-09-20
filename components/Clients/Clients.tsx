5bc515ed595189a80e30960e670981731eee38ba
Kevin van der Werff <kevinsebastianate@hotmail.com>
2021-09-20 15:31:01 +0200

    add alt tag to client img

--- components/Clients/Clients.tsx
+++ components/Clients/Clients.tsx
@@ -9,7 +9,7 @@ const Clients = ({ clients }: { clients: Client[] }) => {
       <p>We value a great working relationship with our clients above all else. It’s why they often come to our parties. It’s also why we’re able to challenge and inspire them to reach for the stars.</p>
       <div className={styles["logos-wrapper"]}>
         {clients.map((client) => (
-          <img className={styles["client-logo"]} src={client.logo} key={client.meta.id}/>
+          <img alt={"logo of " + client.client} className={styles["client-logo"]} src={client.logo} key={client.meta.id}/>
         ))}
       </div>
     </section>
