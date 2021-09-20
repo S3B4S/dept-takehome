9f67ecc5b02d08dbc4c7dfceabd4826f7c8008a3
Kevin van der Werff <kevinsebastianate@hotmail.com>
2021-09-20 22:35:39 +0200

    responsive hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,20 +2,20 @@ import styles from "./Hero.module.css"
 import Image from "next/image"
 import { useState } from "react"
 
-const Hero = ({ onMenuClick }: { onMenuClick: () => void }) => {
+const Hero = ({ onMenuClick }: { onMenuClick: () => void }) => {  
   return (
     <header className={styles.hero}>
       <div className={styles.container}>
-        <div>
-          <nav className={styles.nav}>
-            <Image src="/icons/icon-dept.svg" alt="Dept logo" height="20px" width="60px"/>
-            <a className={styles["nav-items"]} onClick={onMenuClick}>
-              <span className={styles["menu-label"]}>Menu</span>
-              <Image src="/icons/icon-menu.svg" alt="Menu" height="10px" width="20px" />
-            </a>
-          </nav>
-          <span className={styles.callout}>Work</span>
-        </div>
+        <nav className={styles.nav}>
+          <Image src="/icons/icon-dept.svg" alt="Dept logo" height="20px" width="60px"/>
+          <a className={styles["nav-items"]} onClick={onMenuClick}>
+            <span className={styles["menu-label"]}>Menu</span>
+            <Image src="/icons/icon-menu.svg" alt="Menu" height="10px" width="20px" />
+          </a>
+        </nav>
+
+        <span className={styles.callout}>Work</span>
+
         <div className={styles["CTA-wrapper"]}>
           <button className={styles.CTA}>View case</button>
         </div>
