1a7d5aa8a90c66d0a48f265324d4be063803698d
Kevin van der Werff <kevinsebastianate@hotmail.com>
2021-09-20 15:25:49 +0200

    add form

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -3,7 +3,26 @@ import styles from "./ContactForm.module.css"
 const ContactForm = () => {
   return (
     <section className={styles["contact-form"]}>
-      ContactForm
+      <span className={styles["contact-form-label"]}>
+        Questions?<br/>
+        We are here to help!
+      </span>
+      <form action="">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" required/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" required/>
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" required/>
+        </div>
+        <div></div>
+        <button>Send</button>
+      </form>
     </section>
   )
 }
